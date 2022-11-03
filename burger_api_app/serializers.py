2bebf08f045feb93ad15578b5aa721b6c4efa09f
DevXChardike <chardikeltd@gmail.com>
2022-11-03 10:39:28 +0600

    Create a userProfile Serializer

---
+++ burger_api_app/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+from burger_api_app.models import UserProfile
+
+# Create a UserProfileSerializer to show json data 
+class UserProfileSerializer(ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "id",
+            "email",
+            "password",
+        )
+        # to change password i allow write only 
+        extra_kwargs ={
+            'password' : {'write_only': True, 'style': { 'input_type': 'password'}}
+        }
+
+    # Override the default create function as we used custom user model 
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        return user 
