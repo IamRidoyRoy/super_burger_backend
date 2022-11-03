edccadef23c36f27ba98a9b77c7740ecc38438de
DevXChardike <chardikeltd@gmail.com>
2022-11-03 11:44:29 +0600

    install jwt and setup

--- burger_api_app/urls.py
+++ burger_api_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path 
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from burger_api_app.views import UserProfileViewSet
 
 router = DefaultRouter()
 router.register(r'user', UserProfileViewSet)
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls 
