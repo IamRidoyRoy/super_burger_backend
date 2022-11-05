8d16759ae8e52a799dcb178d8d8dcb92e47f75d3
DevXChardike <chardikeltd@gmail.com>
2022-11-05 12:05:43 +0600

    Add order filtering option by id

--- burger_api_app/views.py
+++ burger_api_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from burger_api_app.models import UserProfile, Order, Address
 from burger_api_app.serializers import UserProfileSerializer, OrderSerializer, AddressSeriaLizer
@@ -12,7 +13,18 @@ class UserProfileViewSet(ModelViewSet):
 # Create a OrderViewSet
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    permission_classes = [
+        # IsAuthenticated
+    ]
+
+    # Override the default queryset filtering 
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        id = self.request.query_params.get("id", None)
+        if id is not None:
+            queryset = queryset.filter(user__id=id)
+
+        return queryset
 
 # Create a Adderess ViewSets 
 class AddressViewSets(ModelViewSet):
